3e20d34eed536bfef98afba3c7eccb310acd8a75
Bade om <71856738+ombade@users.noreply.github.com>
2023-10-06 10:31:03 +0530

    .

---
+++ DAA/ASS2/stock.cpp
@@ -0,0 +1,10 @@
+// Implement a problem of maximize Profit by trading 
+// stocks based on given rate per day.
+
+
+// Statement: Given an array arr[] of N positive integers which denotes the cost of selling and buying a stock on 
+// each of the N days. The task is to find the maximum profit that can be earned by buying a stock on or selling 
+// all previously bought stocks on a particular day.
+// Input:
+//  arr[] = {2, 3, 5} Output: 5
+// Input: arr[] = {8, 5, 1} Output: 0
